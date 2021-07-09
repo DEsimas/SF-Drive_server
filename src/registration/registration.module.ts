4741436306969045f00ddf06dfa124efb4723cab
DEsimas <simasuh@gmail.com>
2021-07-09 13:56:34 +0300

    installing components

--- src/registration/registration.module.ts
+++ src/registration/registration.module.ts
@@ -3,10 +3,11 @@ import { RegistrationController } from "./registration.controller";
 import { RegistrationService } from "./registration.service";
 import { RegistrationRepository } from "./repositories/registration.repository";
 import { UserResolver } from './user/user.resolver';
+import { MessagesGateway } from './messages.gateway';
 
 
 @Module({
-    providers: [RegistrationService, RegistrationRepository, UserResolver],
+    providers: [RegistrationService, RegistrationRepository, UserResolver, MessagesGateway],
     controllers: [RegistrationController],
 })
 export class RegistrationModule {};
