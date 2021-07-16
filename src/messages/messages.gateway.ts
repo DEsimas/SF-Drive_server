9301ddc0676618ca7f83795d403e895762640fa5
DEsimas <simasuh@gmail.com>
2021-07-16 12:17:52 +0300

    array replaced with map, added disconnect handler

--- src/messages/messages.gateway.ts
+++ src/messages/messages.gateway.ts
@@ -1,6 +1,8 @@
-import { SubscribeMessage, WebSocketGateway } from '@nestjs/websockets';
+import { OnGatewayDisconnect, SubscribeMessage, WebSocketGateway } from '@nestjs/websockets';
 import { message } from 'src/entities/message.entity';
 import { MessagesService } from './messages.service';
+import { Socket } from 'socket.io';
+import { ObjectID } from 'mongodb';
 const URL = "http://localhost:4200";
 
 @WebSocketGateway(2000, {cors: {
@@ -8,29 +10,42 @@ const URL = "http://localhost:4200";
   methods: ["GET", "POST"],
   credentials: true
 }})
-export class MessagesGateway {
+export class MessagesGateway implements OnGatewayDisconnect {
+
+  private users = new Map<ObjectID, Socket>();
 
   constructor(private readonly MessagesService: MessagesService){
     this.MessagesService.attachSender = function(message) {
       console.log(message);
+
+      this.users.forEach(({userID, socket}) => {
+        if(userID == message.senderID || userID == message.receiverID) { 
+          socket.emit('message', message);
+        }
+      })
     };
+  }
+
+  handleDisconnect(socket: any) {
+    console.log("UwU");
+    this.users.forEach((data, key) => {
+      if(socket == data) this.users.delete(key)
+    });
+    console.log(this.users);
   };
 
-  private users = [];
-
   @SubscribeMessage('auth')
-  auth(client: any, payload: any) {
+  auth(client: Socket, payload: any) {
     if(payload === null) return {event: 'auth', data: { success: false, msg: 'no user in payload' }};
 
     let exist = false;
-    this.users.map((elem, index) => {
-      if(elem._id == payload._id) {
-        this.users[index] = payload;
+    this.users.forEach((elem, index) => {
+      if(elem == payload._id) {
         exist = true;
       }
     });
-    
-    if(!exist) this.users.push(payload);
+
+    if(!exist) this.users.set(payload._id, client);
     
     console.log(this.users);
 
